7bbd67c24975a978444ce7540ee2354393ebc6ba
danhphan <danh.phan.mq@gmail.com>
2022-04-10 21:32:04 +1000

    create get_tickers func

--- pystocks/data.py
+++ pystocks/data.py
@@ -1,11 +1,14 @@
 import pandas as pd
-from pandas_datareader import data
+from pandas_datareader import data as pdr_data
 
 
 def get_tickers(tickers, start_date, end_date):
-    
+    def get_data(ticker):
+        return(pdr_data.get_data_yahoo(ticker, start=start_date, end=end_date))
 
-df = data.get_data_yahoo("MSFT")
+    dfs = map(get_data, tickers)
+    return pd.concat(dfs, keys=tickers, names=['Ticker', 'Date'])
+df = pdr_data.get_data_yahoo("MSFT")
 
 print(df.shape)
 
