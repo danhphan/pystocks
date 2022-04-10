555dc03f75234b8b6bba1f354efaa01ad1081912
danhphan <danh.phan.mq@gmail.com>
2022-04-10 21:39:37 +1000

    update and save datasets

--- pystocks/data.py
+++ pystocks/data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from pandas_datareader import data as pdr_data
-
+import datetime
 
 def get_tickers(tickers, start_date, end_date):
     def get_data(ticker):
@@ -8,8 +8,13 @@ def get_tickers(tickers, start_date, end_date):
 
     dfs = map(get_data, tickers)
     return pd.concat(dfs, keys=tickers, names=['Ticker', 'Date'])
-df = pdr_data.get_data_yahoo("MSFT")
 
-print(df.shape)
+startdate = datetime.datetime(2015,1,1)
+enddate = datetime.datetime(2022,1,1)
+tickers = ["FB", "AMZN", "AAPL", "NFLX", "GOOD"]
+dfs = get_tickers(tickers, start_date=startdate, end_date=enddate)
+
+dfs.to_csv("./data/tickers.csv")
+print(dfs.shape)
 
 
